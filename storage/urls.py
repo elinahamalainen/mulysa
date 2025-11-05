ad51e38b3d1615c23f8229b5113d83baaf14e9c1
Elina Hämäläinen <annaelinah@hotmail.com>
2025-11-05 22:25:51 +0200

    Add basic table structure for storage units

--- storage/urls.py
+++ storage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from storage import views
 
 urlpatterns = [
-    path("", views.storage_home, name="storage_home"),
+    path("", views.reservation_table, name="reservation_table"),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models.storage_unit import StorageUnit
 
-def storage_home(request):
-    return HttpResponse("Storage will be here.")
+"""def storage_home(request):
+    return HttpResponse("Storage will be here.")"""
+
+def reservation_table(request):
+    return render(request, "storage/reservation_table.html", )
